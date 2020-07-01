35f2cbfa0fa37dc501d1fac48beab3b7474f02fe
jennlyle <jennifer.g.lyle@gmail.com>
2020-07-01 09:52:43 +0100

    Added Logos, Started Header Footer, Elaborated HomePage

---
+++ src/Pages/Footer/Footer.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Footer = () => (
+    <>
+        <table>
+            <tbody>
+                <tr>
+                    <td>
+                        [AppiMeals Logo]
+                    </td>
+                    <td>
+                        [contact us button]
+                    </td>
+                    <td>
+                        [about us & faq button]
+                    </td>
+                    <td>
+                        [blog button]
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Socials
+                    </td>
+                    <td colSpan="3">
+                        &nbsp;
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
+    </>
+);
+
+export default Footer;
