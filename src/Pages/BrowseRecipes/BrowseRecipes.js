0b24c3bfa78d334fbd5628c5cabedc3342698cee
iliyand <iliqn1994@gmail.com>
2020-07-03 11:40:17 +0100

    Browse Recipes page structure

---
+++ src/Pages/BrowseRecipes/BrowseRecipes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+import '../BrowseRecipes/BrowseRecipes.css';
+
+// import Image from 'react-bootstrap/Image';
+// import {Container, Row, Col, Button} from 'react-bootstrap';
+
+import { Link } from 'react-router-dom';
+
+const BrowseRecipes = () => (
+    <>
+    <h1>BrowseRecipes</h1>
+    </>
+);
+
+export default BrowseRecipes;
