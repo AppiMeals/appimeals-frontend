dbb8b02de2feff744a6d30ab713e2b7a292b6291
jennlyle <jennifer.g.lyle@gmail.com>
2020-07-05 21:13:25 +0100

    Correcting some things

--- src/Components/RecipeCard/RecipeCard.js
+++ src/Components/RecipeCard/RecipeCard.js
@@ -2,25 +2,15 @@ import React from 'react';
 import { Card, Button } from 'react-bootstrap';
 
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 62ea260a170d06b70424dc9743dd6cd8aff106f4
 const RecipeCard = (props) => {
     return (
     <>
         <Card className="text-center" style={{ width: '18rem' }}>
             <Card.Img variant="top" src={props.image} />
             <Card.Body>
-<<<<<<< HEAD
                 <Card.Title>Card Title</Card.Title>
                 <Card.Text> 
                   
-=======
-                <Card.Title>{props.title}</Card.Title>
-                <Card.Text>
-                    {props.servings}
->>>>>>> 62ea260a170d06b70424dc9743dd6cd8aff106f4
                 </Card.Text>
 
                 <Card.Link href="#">Card Link</Card.Link>
