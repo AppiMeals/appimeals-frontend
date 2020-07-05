feef433ea0c1224065eda63b5e6baec718fa96a2
iliyand <iliqn1994@gmail.com>
2020-07-05 16:58:13 +0100

    cleared an error

--- src/App.js
+++ src/App.js
@@ -10,10 +10,6 @@ import Registration from './Pages/Registration/Registration';
 import SignInPage from './Pages/SignInPage/SignInPage';
 import MyMealShop from './Pages/MyMealShop/MyMealShop';
 import SuperMarketLogIn from './Pages/SuperMarketLogIn/SuperMarketLogIn';
-
-
-ajshashasoiuffjbg
-
 import LoginExample from './Pages/LoginExample/LoginExample';
 
 import NavBar from './Components/NavBar/NavBar';
