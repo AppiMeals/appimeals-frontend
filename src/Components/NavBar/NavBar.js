9c8fa3e4a953001720d60086343c56d1d91e1dce
iliyand <iliqn1994@gmail.com>
2020-07-06 21:12:22 +0100

    Browse recipes page improvements.

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -5,9 +5,11 @@ Link search box to Spoonacular API
 */
 
 import React from 'react';
-import {Navbar, Nav, FormControl, Button, Form} from 'react-bootstrap';
+import {Navbar, Nav} from 'react-bootstrap';
 import Logo from '../../Images/logo5-200x200.png';
 import { Link } from 'react-router-dom';
+
+
 
 
  const NavBar = () => {
@@ -16,11 +18,10 @@ import { Link } from 'react-router-dom';
     <header>
         <Navbar bg="dark" variant="dark">
             <Link to="/">
-
                 <img
                     src={Logo}
-                    width="50"
-                    height="50"
+                    width="50px"
+                    height="50px"
                     className="d-inline-block align-top"
                     alt="AppiMeals Logo"
                 />
@@ -29,10 +30,6 @@ import { Link } from 'react-router-dom';
             <Nav className="mr-auto">
                 <Nav.Link href="/SignIn">Sign In</Nav.Link>
             </Nav>
-            <Form inline>
-                <FormControl type="text" placeholder="Recipe" className="mr-sm-2" />
-                <Button variant="outline-primary">Search</Button>
-            </Form>
         </Navbar>
     </header>
     </>
--- src/Components/RecipeCard/RecipeCard.js
+++ src/Components/RecipeCard/RecipeCard.js
@@ -1,31 +1,33 @@
 import React from 'react';
-import { Card, Button } from 'react-bootstrap';
-import parse from 'html-react-parser';
+import { Card, Button, CardGroup } from 'react-bootstrap';
+//import parse from 'html-react-parser';
 
+import './RecipeCard.css';
 
 
 const RecipeCard = (props) => {
     return (
-    <>
-        <Card className="text-center" style={{ width: '20vw' }}>
+    <>  
+    <CardGroup className="card-container">
+        <Card className="text-center" >
             <Card.Img variant="top" src={props.image} />
             <Card.Body>
                 <Card.Title>{props.title}</Card.Title>
                 <Card.Text>
-                    <span>Calories: {props.calories}</span>
+                    <span>Calories: {Math.round(props.calories)}</span>
                     <br />
                     <span>Cooking Time: {props.cookingTime}min</span>
                     <br />
                     <span>Servings: {props.servings}</span>
                     <br />
-                    <span>{parse(props.summary)}</span>
                 </Card.Text>
 
-                <Card.Link href="#">Card Link</Card.Link>
+                <Card.Link href={props.url} target={"_blank"} >View Recipe</Card.Link>
                 <br />
                 <Button variant="primary">Add to myMeals</Button>
             </Card.Body>
         </Card>
+        </CardGroup>
     </>
     )};
 
