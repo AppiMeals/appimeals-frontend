92ce641d7c3b09fdd898c86efd875c69770287f8
iliyand <iliqn1994@gmail.com>
2020-07-12 11:07:55 +0100

    hub page styling

--- src/Components/UpComingMealsCard/UpcomingMealsCard.js
+++ src/Components/UpComingMealsCard/UpcomingMealsCard.js
@@ -1,94 +1,32 @@
 import React from 'react';
 import { CardGroup, Card, Button, Image } from 'react-bootstrap';
 
+import '../UpComingMealsCard/UpcomingMealsCard.css';
 
 const MyUpcomingMealsCard = (props) => {
+
+
+  const handleDeleteClick = () => {
+    props.deleteRecipe(props.id);
+}
+
+
   return (
     <>
       <header>
         <CardGroup className="card-group">
-          <Card>
-            <Card.Img variant="top" src="holder.js/100px160" />
+
+        <Card className="card-item">
+            <Image className="card-image" src={props.image}  />
+            <Button className="close-button" onClick={handleDeleteClick}>X</Button>
             <Card.Body>
-              <Card.Title>Monday</Card.Title>
-              <Card.Text>
-                Some example text
-      </Card.Text>
+              <Card.Title>{props.day}</Card.Title>
+              <Card.Text className="text-field">
+                {props.title}
+              </Card.Text>
             </Card.Body>
             <Card.Footer>
-              <Button variant="primary">View Full Recipe</Button>
-            </Card.Footer>
-          </Card>
-          <Card>
-            <Image className="imageT" src={props.image}  />
-            <Card.Body>
-              <Card.Title>Tuesday</Card.Title>
-              <Card.Text>
-                Some example text
-      </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <Button variant="primary">View Full Recipe</Button>
-            </Card.Footer>
-          </Card>
-          <Card>
-            <Card.Img variant="top" src="holder.js/100px160" />
-            <Card.Body>
-              <Card.Title>Wednesday</Card.Title>
-              <Card.Text>
-                Some example text
-      </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <Button variant="primary">View Full Recipe</Button>
-            </Card.Footer>
-          </Card>
-          <Card>
-            <Card.Img variant="top" src="holder.js/100px160" />
-            <Card.Body>
-              <Card.Title>Thursday</Card.Title>
-              <Card.Text>
-                Some example text
-      </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <Button variant="primary">View Full Recipe</Button>
-            </Card.Footer>
-          </Card>
-          <Card>
-            <Card.Img variant="top" src="holder.js/100px160" />
-            <Card.Body>
-              <Card.Title>Friday</Card.Title>
-              <Card.Text>
-                Some example text
-      </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <Button variant="primary">View Full Recipe</Button>
-            </Card.Footer>
-          </Card>
-          <Card>
-            <Card.Img variant="top" src="holder.js/100px160" />
-            <Card.Body>
-              <Card.Title>Saturday</Card.Title>
-              <Card.Text>
-                Some example text
-      </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <Button variant="primary">View Full Recipe</Button>
-            </Card.Footer>
-          </Card>
-          <Card>
-            <Card.Img variant="top" src="holder.js/100px160" />
-            <Card.Body>
-              <Card.Title>Sunday</Card.Title>
-              <Card.Text>
-                Some example text
-      </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <Button variant="primary">View Full Recipe</Button>
+              <Card.Link href={props.url} >View Full Recipe</Card.Link>
             </Card.Footer>
           </Card>
 
